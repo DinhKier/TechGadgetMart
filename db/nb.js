25e9f119e862ed11539798c95b4509006f3e6af9
DinhKier <trantrankiet59@gmail.com>
2024-07-29 19:24:08 +0700

    init project

---
+++ db/nb.js
@@ -0,0 +1,57 @@
+[
+    {
+        "title": "Xây dựng trang web",
+        "image": "https://d1nzpkv5wwh1xf.cloudfront.net/640/k-57bd1aa7047c994a15e41aca/20180704-/792484_cc98_3.jpg",
+        "link": "#"
+    },
+    {
+        "title": "Lập trình Python căn bản",
+        "image": "https://d1nzpkv5wwh1xf.cloudfront.net/640/k-57bd1aa7047c994a15e41aca/20180704-/792484_cc98_3.jpg",
+        "link": "#"
+    },
+    {
+        "title": "Học lập trình Java từ cơ bản đến nâng cao",
+        "image": "https://d1nzpkv5wwh1xf.cloudfront.net/640/k-57bd1aa7047c994a15e41aca/20180704-/792484_cc98_3.jpg",
+        "link": "#"
+    },
+    {
+        "title": "Machine Learning cơ bản",
+        "image": "https://d1nzpkv5wwh1xf.cloudfront.net/640/k-57bd1aa7047c994a15e41aca/20180704-/792484_cc98_3.jpg",
+        "link": "#"
+    },
+    {
+        "title": "Deep Learning và ứng dụng",
+        "image": "https://d1nzpkv5wwh1xf.cloudfront.net/640/k-57bd1aa7047c994a15e41aca/20180704-/792484_cc98_3.jpg",
+        "link": "#"
+    },
+    {
+        "title": "Cơ bản về Data Science",
+        "image": "https://d1nzpkv5wwh1xf.cloudfront.net/640/k-57bd1aa7047c994a15e41aca/20180704-/792484_cc98_3.jpg",
+        "link": "#"
+    },
+    {
+        "title": "Lập trình ReactJS từ A đến Z",
+        "image": "https://d1nzpkv5wwh1xf.cloudfront.net/640/k-57bd1aa7047c994a15e41aca/20180704-/792484_cc98_3.jpg",
+        "link": "#"
+    },
+    {
+        "title": "Angular cơ bản và nâng cao",
+        "image": "https://d1nzpkv5wwh1xf.cloudfront.net/640/k-57bd1aa7047c994a15e41aca/20180704-/792484_cc98_3.jpg",
+        "link": "#"
+    },
+    {
+        "title": "Cấu trúc dữ liệu và giải thuật trong Python",
+        "image": "https://d1nzpkv5wwh1xf.cloudfront.net/640/k-57bd1aa7047c994a15e41aca/20180704-/792484_cc98_3.jpg",
+        "link": "#"
+    },
+    {
+        "title": "Lập trình Android căn bản",
+        "image": "https://d1nzpkv5wwh1xf.cloudfront.net/640/k-57bd1aa7047c994a15e41aca/20180704-/792484_cc98_3.jpg",
+        "link": "#"
+    },
+    {
+        "title": "Phát triển ứng dụng di động với Flutter",
+        "image": "https://d1nzpkv5wwh1xf.cloudfront.net/640/k-57bd1aa7047c994a15e41aca/20180704-/792484_cc98_3.jpg",
+        "link": "#"
+    }
+]
